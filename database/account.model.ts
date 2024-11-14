3328ba233a51931b4434ec47acb0523226ae5260
NguyenTrongAnh <anhnguyen20040312@gmail.com>
2024-11-14 10:30:17 +0700

    1

--- database/account.model.ts
+++ database/account.model.ts
@@ -4,8 +4,8 @@ export interface IAccount {
   userId: Types.ObjectId;
   name: string;
   image?: string;
-  password?: string;
-  provider: string;
+  password?: string; // Thuộc tính tuỳ chọn
+  provider: string; // Thuộc tính bắt buộc
   providerAccountId: string;
 }
 
@@ -19,6 +19,7 @@ const AccountSchema = new Schema<IAccount>(
     providerAccountId: { type: String, required: true },
   },
   { timestamps: true }
+  // Với { timestamps: true }, khi một tài liệu User mới được tạo, MongoDB sẽ tự động thêm createdAt và updatedAt. Nếu tài liệu được cập nhật sau đó, chỉ updatedAt sẽ được cập nhật với thời gian mới, trong khi createdAt vẫn giữ nguyên thời gian ban đầu.
 );
 
 const Account = models?.Account || model<IAccount>("Account", AccountSchema);
--- database/user.model.ts
+++ database/user.model.ts
@@ -5,8 +5,8 @@ export interface IUser {
   username: string;
   email: string;
   bio?: string;
-  image: string;
-  location?: string;
+  image: string; //Thuộc tính bắt buộc
+  location?: string; //Thuộc tính tuỳ chọn
   portfolio?: string;
   reputation?: number;
 }
@@ -23,8 +23,11 @@ const UserSchema = new Schema(
     reputation: { type: Number, default: 0 },
   },
   { timestamps: true }
+  // Với { timestamps: true }, khi một tài liệu User mới được tạo, MongoDB sẽ tự động thêm createdAt và updatedAt. Nếu tài liệu được cập nhật sau đó, chỉ updatedAt sẽ được cập nhật với thời gian mới, trong khi createdAt vẫn giữ nguyên thời gian ban đầu.
 );
 
-const User = models?.user || model<IUser>("User", UserSchema);
-
+const User = models?.user || model<IUser>("User", UserSchema); // <IUser> là kiểu dữ liệu TypeScript của mô hình, xác định rằng mô hình User này phải tuân theo cấu trúc được định nghĩa trong interface IUser
+// Sử dụng models?.user để kiểm tra xem mô hình đã tồn tại hay chưa
+// Nếu mô hình User chưa tồn tại, tạo mới bằng cách gọi model<IUser>("User", UserSchema).
+// Tạo ra một mô hình Mongoose có tên User dựa trên cấu trúc của UserSchema và sử dụng kiểu dữ liệu TypeScript IUser để đảm bảo tính an toàn về kiểu trong quá trình làm việc với mô hình này.
 export default User;
